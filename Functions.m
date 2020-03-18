5c3d936667e5996fc035dccfaf11bd5eaca2c75e
GabrielRalph <gltralph@gmail.com>
2020-03-18 20:34:58 +1100

    breif plus images

--- Functions.m
+++ Functions.m
@@ -21,14 +21,20 @@ for i = velocity_range
     end
     idx = idx + 1;
 end
-subplot(2,2,1);
-plot(velocity_range, stop_distance_A, velocity_range, stop_distance_B);
 
+plot(velocity_range, stop_distance_A, velocity_range, stop_distance_B);
+title('Stopping distance');
+ylabel('Stopping distance (m)');
+xlabel('Initial velocity (m/s)');
+refline([0 48]);
+saveas(gcf, 'stop-distance-vs-initial-velocit.svg');
+clf;
 dataA = simulateStop(soln_A(1), 'A');
 dataB = simulateStop(soln_B(1), 'B');
-subplot(2,2,2);
+
+subplot(3,1,1);
 plot(dataA(:, 1), dataA(:, 2),dataB(:, 1), dataB(:, 2));
-subplot(2,2,3);
+subplot(3,1,2);
 plot(dataA(:, 1), dataA(:, 3),dataB(:, 1), dataB(:, 3));
-subplot(2,2,4);
+subplot(3,1,3);
 plot(dataA(:, 2), dataA(:, 4),dataB(:, 2), dataB(:, 4));
