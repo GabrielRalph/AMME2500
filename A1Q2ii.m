b59cd01b3caf72bdd69dbb0e4c69353880c9ef14
GabrielRalph <gltralph@gmail.com>
2020-03-18 22:09:26 +1100

    solve a v too

--- A1Q2ii.m
+++ A1Q2ii.m
@@ -1,5 +1,14 @@
+r = 100;
 syms t;
-theta_30 = (exp(t) - t - 1)/200 == pi/6;
+theta_30 = 0.5*(exp(t) - t - 1)/r == pi/6;
 rule = t > 0;
 solution = solve([theta_30 rule]);
-fprintf('%s = %.4d\n', solution, double(solution));
+t = double(solution);
+fprintf('t = %s = %.4d\n\n', solution, t);
+
+
+v_t = 0.5*(exp(t) - 1);
+a_t = 0.5*exp(t);
+a_c = v_t^2/r;
+a = sqrt(a_t^2 + a_c^2);
+fprintf('v = %.5g and a = %.5g\n\n', v_t, a);
