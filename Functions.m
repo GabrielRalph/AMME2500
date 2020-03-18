914edf4cd1c488e51de572607694c2a19260027d
GabrielRalph <gltralph@gmail.com>
2020-03-18 20:39:24 +1100

    updates

--- Functions.m
+++ Functions.m
@@ -21,17 +21,17 @@ for i = velocity_range
     end
     idx = idx + 1;
 end
-
+f = figure();
 plot(velocity_range, stop_distance_A, velocity_range, stop_distance_B);
 title('Stopping distance');
 ylabel('Stopping distance (m)');
 xlabel('Initial velocity (m/s)');
 refline([0 48]);
-saveas(gcf, 'stop-distance-vs-initial-velocit.svg');
-clf;
+saveas(f, 'stop-distance-vs-initial-velocit.svg');
+
 dataA = simulateStop(soln_A(1), 'A');
 dataB = simulateStop(soln_B(1), 'B');
-
+f2 = figure();
 subplot(3,1,1);
 plot(dataA(:, 1), dataA(:, 2),dataB(:, 1), dataB(:, 2));
 subplot(3,1,2);
